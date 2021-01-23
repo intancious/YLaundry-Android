d1e3bbadce31e33eb1777b443aba5c1c14ce1035
intancious <intancious98@gmail.com>
2021-01-23 17:34:07 +0700

    first commit

---
+++ app/src/main/java/com/example/ylaundry/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.ylaundry;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class SplashScreen extends AppCompatActivity {
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L); //3000 L = 3 detik
+    }
+}
